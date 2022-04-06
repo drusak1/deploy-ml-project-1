ff14c709f5e110e21c38c3ede3e4e6879b155e83
Dima Rusak <rusakda@mail.ru>
2022-04-06 10:02:59 +0300

    add structure for project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='practice deploy ml models in production way',
+    author='Daillek',
+    license='',
+)
